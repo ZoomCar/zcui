7b001a0c2082b85d7979137072c9fc6c6510c1b2
Hamid Raza <contact@hamidraza.net>
2017-03-29 13:48:04 +0530

    added create command test cases

--- tests/commands/create/component.js
+++ tests/commands/create/component.js
@@ -29,3 +29,50 @@ Examples:
   });
 });
 
+test("CREATE_COMPONENT: success", t => {
+  const command = `${zcui} zc-calendar`;
+
+  /**
+   * Cleanup zc-calendar component
+   */
+  shell.rm('-rf', 'src/components/zc-calendar', {silent: true});
+
+  shell.exec(command, {silent:true}, (code, stdout, stderr) => {
+    if(code === 1) t.fail(stderr);
+    t.equal(stdout.trim(), `✔ zc-calendar Component created
+
+  Use:
+  import ZcCalendar from '~/components/zc-calendar';
+    `.trim());
+    t.end();
+  });
+});
+
+test("CREATE_COMPONENT: has component files", t => {
+
+  /**
+   * Don not remove zc-calendar component
+   * zc-calendar component created in previous test
+   */
+  t.ok(shell.test('-f', 'src/components/zc-calendar/index.js'));
+  t.ok(shell.test('-f', 'src/components/zc-calendar/zc-calendar.js'));
+  t.ok(shell.test('-f', 'src/components/zc-calendar/zc-calendar.vue'));
+  t.ok(shell.test('-f', 'src/components/zc-calendar/zc-calendar.scss'));
+  t.end();
+});
+
+test("CREATE_COMPONENT: error on duplicate", t => {
+  const command = `${zcui} zc-calendar`;
+
+  /**
+   * Don not remove zc-calendar component
+   * zc-calendar component created in previous test
+   */
+
+  shell.exec(command, {silent:true}, (code, stdout, stderr) => {
+    if(code === 0) t.fail(stderr);
+    t.equal(stdout.trim(), '✖ zc-calendar component already exits! Please choose some another name!!!');
+    t.end();
+  });
+});
+
--- tests/commands/create/helper.js
+++ tests/commands/create/helper.js
@@ -29,3 +29,48 @@ Examples:
   });
 });
 
+test("CREATE_HELPER: success", t => {
+  const command = `${zcui} date`;
+
+  /**
+   * Cleanup date helper
+   */
+  shell.rm('-rf', 'src/helpers/date.js', {silent: true});
+
+  shell.exec(command, {silent:true}, (code, stdout, stderr) => {
+    if(code === 1) t.fail(stderr);
+    t.equal(stdout.trim(), `✔ date Helper created
+
+  Use:
+  import date from '~/helpers/date';
+    `.trim());
+    t.end();
+  });
+});
+
+test("CREATE_HELPER: has helper files", t => {
+
+  /**
+   * Don not remove date helper
+   * date helper created in previous test
+   */
+  t.ok(shell.test('-f', 'src/helpers/date.js'));
+  t.ok(shell.test('-f', 'src/helpers/__tests__/date.spec.js'));
+  t.end();
+});
+
+test("CREATE_HELPER: error on duplicate", t => {
+  const command = `${zcui} date`;
+
+  /**
+   * Don not remove date helper
+   * date helper created in previous test
+   */
+
+  shell.exec(command, {silent:true}, (code, stdout, stderr) => {
+    if(code === 0) t.fail(stderr);
+    t.equal(stdout.trim(), '✖ date helper already exits! Please choose some another name!!!');
+    t.end();
+  });
+});
+
